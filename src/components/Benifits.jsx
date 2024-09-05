369882e9af93f449470705262444c9eeeb2c8b24
Sanyam Pandey <sanyam071104@gmail.com>
2024-09-05 22:04:46 +0530

    Pushing changes

---
+++ src/components/Benifits.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Benifits = () => {
+  return (
+    <div className='mx-[121.12px] flex flex-col'>
+
+        <div className='flex flex-row'>
+
+        </div>
+        
+        <div className='flex flex-row'>
+
+        </div>
+    
+    </div>
+  )
+}
+
+const Card = ({}) => {
+
+}
+
+export default Benifits
