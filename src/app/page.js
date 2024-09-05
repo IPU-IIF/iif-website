227ee9c053041c73cf5942018e8dab1d68071b62
sabhya-beginner <agarwalsabhya44@gmail.com>
2024-09-05 22:02:35 +0530

    Pushing new changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import DataCounter from "@/components/DataCounter";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <DataCounter />
+      <Footer />
     </>
   );
 }
