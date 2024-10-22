a8043a38df128fd25cf4f44a3be57ea8a70e618e
kriti ayush <kritiayush@Air.local>
2024-10-23 00:07:54 +0530

    dlt benefits and navbar coz changed on main

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Benefits from "@/components/Benefits";
-import DataCounter from "@/components/DataCounter";
-import Footer from "@/components/Footer";
-// import NavBar from "@/components/NavBar";
+import NavBar from "@/components/NavBar";
 import Overview from "@/components/Overview";
 import HeroSection from "@/components/HeroSection";
 
@@ -9,12 +7,11 @@ export default function Home() {
   return (
     <>
 
-      {/* <NavBar /> */}
+      <NavBar />
       <HeroSection />
-      <DataCounter />
       <Overview />
       <Benefits />
-      <Footer />
+      
 
     </>
   );
